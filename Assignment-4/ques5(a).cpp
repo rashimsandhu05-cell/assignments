99f0dfaa63c7ae904afe70cb53e277c24778d092
rashim kaur <rashimsandhu05@gmail.com>
2025-09-18 12:03:20 +0530

    ques5(a).cpp

--- Assignment-4/ques5(a).cpp
+++ Assignment-4/ques5(a).cpp
@@ -1,61 +1,45 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define MAX 100
+#include <iostream>
+#include <queue>
+using namespace std;
 
-int q1[MAX], q2[MAX];
-int f1=0, r1=0, f2=0, r2=0;
-
-int q1_empty() { return f1==r1; }
-int q2_empty() { return f2==r2; }
-int q1_size() { return r1 - f1; }
-int q2_size() { return r2 - f2; }
-
-void q1_enqueue(int x) { q1[r1++] = x; }
-int q1_dequeue() { return q1[f1++]; }
-int q1_front() { return q1[f1]; }
-
-void q2_enqueue(int x) { q2[r2++] = x; }
-int q2_dequeue() { return q2[f2++]; }
+queue<int> q1, q2;
 
 void push(int x) {
-    q2_enqueue(x);
-    while (!q1_empty()) q2_enqueue(q1_dequeue());
-    int i=0;
-    for (i=f2;i<r2;i++) q1[i-f2]=q2[i];
-    r1 = r2 - f2;
-    f1 = 0;
-    r1 = r1;
-    f2 = r2 = 0;
+    q2.push(x);
+    while (!q1.empty()) {
+        q2.push(q1.front());
+        q1.pop();
+    }
+    swap(q1, q2);
 }
 
-void pop_stack() {
-    if (q1_empty()) { printf("Stack Underflow\n"); return; }
-    printf("%d popped\n", q1_dequeue());
+void pop() {
+    if (q1.empty()) cout << "Underflow\n";
+    else { cout << q1.front() << " popped\n"; q1.pop(); }
 }
 
-void peek_stack() {
-    if (q1_empty()) { printf("Stack is Empty\n"); return; }
-    printf("Top: %d\n", q1_front());
+void top() {
+    if (q1.empty()) cout << "Empty\n";
+    else cout << q1.front() << "\n";
 }
 
-void display_stack() {
-    if (q1_empty()) { printf("Stack is Empty\n"); return; }
-    for (int i=f1;i<r1;i++) printf("%d ", q1[i]);
-    printf("\n");
+void display() {
+    if (q1.empty()) { cout << "Empty\n"; return; }
+    queue<int> t = q1;
+    while (!t.empty()) { cout << t.front() << " "; t.pop(); }
+    cout << "\n";
 }
 
 int main() {
-    int choice, val;
+    int ch, x;
     while (1) {
-        printf("\n1.Push 2.Pop 3.Peek 4.Display 5.Exit\n");
-        if (scanf("%d", &choice)!=1) return 0;
-        switch (choice) {
-            case 1: scanf("%d", &val); push(val); break;
-            case 2: pop_stack(); break;
-            case 3: peek_stack(); break;
-            case 4: display_stack(); break;
-            case 5: return 0;
-            default: printf("Invalid choice\n");
-        }
+        cout << "1.Push 2.Pop 3.Top 4.Display 5.Exit\n";
+        cin >> ch;
+        if (ch == 1) { cin >> x; push(x); }
+        else if (ch == 2) pop();
+        else if (ch == 3) top();
+        else if (ch == 4) display();
+        else if (ch == 5) break;
+        else cout << "Invalid\n";
     }
 }
