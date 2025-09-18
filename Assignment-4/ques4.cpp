15b25d4fe4ce3c1bcb70cb48e71374e83d5ad8e7
rashim kaur <rashimsandhu05@gmail.com>
2025-09-18 12:02:07 +0530

    ques4.cpp

--- Assignment-4/ques4.cpp
+++ Assignment-4/ques4.cpp
@@ -1,48 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#define QSIZE 10000
-
-int q[QSIZE];
-int front = 0, rear = 0;
-
-void enqueue(int x) {
-    q[rear++] = x;
-    if (rear == QSIZE) rear = 0;
-}
-
-int isEmpty() {
-    return front == rear;
-}
-
-int peek() {
-    return q[front];
-}
-
-void dequeue() {
-    if (!isEmpty()) {
-        front++;
-        if (front == QSIZE) front = 0;
-    }
-}
+#include <iostream>
+#include <queue>
+using namespace std;
 
 int main() {
+    queue<char> q;
     int freq[256] = {0};
-    int ch;
-    int firstPrinted = 0;
-
-    while ((ch = getchar()) != EOF) {
-        if (ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r') continue;
-        unsigned char c = (unsigned char) ch;
+    char c;
+    while (cin >> c) {
         freq[c]++;
-        enqueue(c);
-        while (!isEmpty() && freq[(unsigned char)peek()] > 1) dequeue();
-        if (firstPrinted) printf(" ");
-        if (isEmpty()) printf("-1");
-        else printf("%c", (char)peek());
-        firstPrinted = 1;
+        q.push(c);
+        while (!q.empty() && freq[q.front()] > 1) q.pop();
+        if (q.empty()) cout << -1 << " ";
+        else cout << q.front() << " ";
     }
-
-    printf("\n");
     return 0;
 }
